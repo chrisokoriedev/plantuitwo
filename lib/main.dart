eb46e7c86ddaddd228e151a72170dc3d98c63626
ChrisIuil <okoriec01@gmail.com>
2022-11-15 06:54:18 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+const kPadding = EdgeInsets.symmetric(horizontal: 20, vertical: 20);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -24,14 +26,20 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
+        padding: kPadding,
+        width: mediaWidth,
         decoration: BoxDecoration(
             image: DecorationImage(
                 colorFilter: ColorFilter.mode(
                     Colors.black.withOpacity(0.3), BlendMode.darken),
                 image: const AssetImage('assets/bg2.jpg'),
                 fit: BoxFit.cover)),
+        child: Column(
+          children: [Text('Green way is out way')],
+        ),
       ),
     );
   }
